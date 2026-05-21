e531cc30d8c86481f689ff542f1843fa1e20f2ad
lissybarrera24 <Lissybuezo1996@gmail.com>
2026-05-20 20:04:13 -0600

    Add files via upload

---
+++ Layout.tsx
@@ -0,0 +1,14 @@
+import "./globals.css";
+import { AppProvider } from "../context/AppContext";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <AppProvider>
+          {children}
+        </AppProvider>
+      </body>
+    </html>
+  );
+}
